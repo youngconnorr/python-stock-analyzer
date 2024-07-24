6f1252d8f8125494aeb9999876e64717111a8d31
youngconnorr <134354392+youngconnorr@users.noreply.github.com>
2024-07-24 13:35:05 -0600

    created visualizations and added a ticker list

--- main.py
+++ main.py
@@ -2,14 +2,7 @@ import yfinance as yf
 import stock_analyzer as cs
 
 if __name__ == "__main__":
-    # apple = yf.Ticker("NVDA")
-    # dividends = apple.dividends
-    # splits = apple.splits
-    # data = stock_analyzer.fetch_stock_history_data("NVDA", "2020-05-10", "2024-07-20")
-    # moving_av = stock_analyzer.moving_average(data, 5)
-    # print(moving_av)
-    AAPL = cs.ChosenStock("AAPL","2020-10-01", "2020-10-11")
+    tickers = ["AAPL", "AMZN", "TSLA"]
     
-    moving_avg = AAPL.moving_average(window=5)
-
-    print(moving_avg)
+    stock = cs.ChosenStock(tickers,"2012-10-01", "2020-10-11")
+    
--- stock_analyzer.py
+++ stock_analyzer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import yfinance as yf
-import matplotlib as plt
-
+import matplotlib.pyplot as plt
+from typing import Optional
 #add interval(1m) for very accurate stock prices
 #if looking at history do 1d
 
@@ -9,36 +9,52 @@ import matplotlib as plt
 
 class ChosenStock:
     
-    ticker = ""
-    start_date = ""
-    end_date = ""
-    stock_data = None
-    
-    def __init__(self, ticker: str, start_date: str, end_date: str):
+    def __init__(self, ticker: list, start_date: str, end_date: str):
         self.ticker = ticker
         self.start_date = start_date
         self.end_date = end_date
-        self.stock_data = self.fetch_stock_history_data()
+        self.run_program()
         
         
-        
-    def fetch_stock_history_data(self) -> pd.DataFrame:
-        fetched_stock_data = yf.download(self.ticker, self.start_date, self.end_date)
-        print(fetched_stock_data)
+    def fetch_stock_history_data(self, ticker: str) -> pd.DataFrame:
+        fetched_stock_data = yf.download(ticker, self.start_date, self.end_date)
         return fetched_stock_data
     
 
-    def moving_average(self, window: int) -> pd.Series:
-        if "Close" not in self.stock_data:
+    def moving_average(self, stock_data: pd.DataFrame, window: int) -> pd.Series:
+        if "Close" not in stock_data:
             raise ValueError("Invalid Stock: need \"Close\" column")
     
-        moving_average = self.stock_data["Close"].rolling(window=window).mean()
-        print(moving_average)
+        moving_average = stock_data["Close"].rolling(window=window).mean()
         return moving_average
-
-
-
-
-
-
-
+    
+    def plot_stock_data(self, stock_data: pd.DataFrame, ticker: str, moving_average: Optional[pd.Series] = None) -> None:
+        plt.plot(stock_data['Close'], color='black', label='Closing Price')
+    
+        if moving_average is not None:
+            plt.plot(moving_average, color='black', label='Closing Price')
+    
+        plt.title(f'{ticker} Stock Price')
+        plt.xlabel('Date')
+        plt.ylabel(f'{ticker} Price')
+        plt.legend()
+        plt.show()
+        
+    def run_program(self) -> None:
+        
+        data_list = []
+        moving_data_list = []
+        
+        for t in self.ticker:
+            data = self.fetch_stock_history_data(t)
+            data_list.append(data)
+            
+            moving_data = self.moving_average(data, 3)
+            moving_data_list.append(moving_data)
+            
+            self.plot_stock_data(data, t, moving_data)
+        
+        
+        
+            
+        
